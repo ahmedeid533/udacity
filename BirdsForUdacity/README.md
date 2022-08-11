d087f22a9c982d433160b92a1f137990074be26d
ahmedeid533 <90480605+ahmedeid533@users.noreply.github.com>
2022-08-11 23:54:22 +0200

    Add files via upload

---
+++ BirdsForUdacity/README.md
@@ -0,0 +1,46 @@
+# BirdsForUdacity
+
+This Project rquired from udacity about page have sections with buttons head to them and have more features written down
+# Table of contents
+
+- [Project Title](#project-title)
+- [Table of contents](#table-of-contents)
+- [Installation](#installation)
+- [Development](#development)
+    - [Adding new features or fixing bugs](#adding-new-features-or-fixing-bugs)
+- [License](#license)
+- [Footer](#footer)
+
+# Installation
+[(Back to top)](#table-of-contents)
+
+simply hit this [(Button)](#table-of-contents) and it will be downloaded as .zip File 
+extarct it then click index.html and hope u enjoy the page.
+
+# Development
+[(Back to top)](#table-of-contents)
+
+this Project have many Features like 
+-slide bar To sellect the section using attriputes mouseover , mouseleave.
+-hover added for every button make background of button white
+-every button head to it's section with smooth scrolling
+-collabsale sections
+-and The flying Bird for more entertainment XD.
+-color for every section relate to the sections name like blue for ice gold for desert etc...
+-lines that sparate the sections 
+-and more u could explore them your self 
+
+### Adding new features or fixing bugs
+[(Back to top)](#table-of-contents)
+
+open for critique and if you see any bugs blease tell me thanks in advance for your time
+
+# License
+[(Back to top)](#table-of-contents)
+
+all credit for udacit , w3school , geeks for geeks and all who give lessons on youtube
+
+# Footer
+[(Back to top)](#table-of-contents)
+
+thanks for Your Time it's Very Valuable so thanks again
---
+++ BirdsForUdacity/js/app.js
@@ -0,0 +1,112 @@
+let bird = document.getElementById("bird");
+let box = document.getElementById("birdbox");
+/* Make a Bird Move Inside The bar */
+let pixls = 0;
+let togle = true;
+let Bird_Anmiation = setInterval(()=> 
+  {
+    /* the Togle Use Is To Know When Change Diraction */
+    if ( togle == true)
+    {
+      pixls = pixls + 2 ;
+      /* To Make The Bird Movement Related to Box width aka To Be Usable For any Device. */
+      if (pixls >= box.clientWidth-80)
+      {togle = false ;
+      bird.style.cssText = 'transform: rotatey(0deg);  ';}
+    }
+
+    if ( togle == false) {
+      pixls = pixls-2;
+      if (pixls <= 0)
+     { togle = true ;
+      bird.style.cssText = 'transform: rotatey(180deg);  ';}
+    }
+    /* move the bird BY pixls */
+    move(pixls);
+  }, 20);
+/* evry 20 ms the bird move 2 px */
+function move(pixls) 
+  {
+  bird.style.left = pixls  + 'px';
+  }
+/* Here We End Bird Movements */ 
+let Sections = document.querySelector("#sections_box");
+let select = document.querySelectorAll(".button");
+let TheBar = document.querySelector("#selector");
+// To Show and Hide Sections Slide Box 
+Sections.addEventListener("mouseover" ,  () =>
+{
+  for (let i = 2 ; i <= 5 ; i++)
+  {
+    select[i].style.display = "block";
+  }
+});
+Sections.addEventListener("mouseleave" , () =>
+{
+  for (let i = 2 ; i <= 5 ; i++)
+  {
+    select[i].style.display = "none"; 
+  }
+});
+// To Let Home Buuton Lead To Top of The Page
+let home = document.querySelector("#home");
+home.addEventListener("click",() => 
+  {
+    scroll(0,0)
+  })
+// To Show The Bar While Going Up And Hide While Going Down
+let yindx = window.scrollY;
+document.addEventListener("scroll",()=>
+{
+  if (yindx >50)
+  {
+      if (yindx >= window.scrollY)
+    {
+      TheBar.classList.remove("hidden")
+    }
+    else if (yindx < window.scrollY)
+    { 
+      TheBar.classList.add("hidden")
+    }
+    }
+    yindx = window.scrollY;
+  })
+// To Collabse The Sections
+// I Put tog To Detrmine When It Open And When Not
+let tog = false ;
+let selections = document.querySelectorAll(".section");
+let Buttons = document.querySelectorAll(".button");
+for (let i = 0 ; i <= 4 ; i++)
+{
+  selections[i].addEventListener("click",() =>
+  {
+    if (tog == false)
+    {
+      selections[i].nextElementSibling.style.display = "none";
+      tog = true ;
+      selections[i].style.cssText = 'box-shadow: 2px 2px 5px rgb(0, 0, 0.2);';
+    }
+    else if (tog == true)
+    {
+      selections[i].nextElementSibling.style.display = "block";
+      tog = false ;
+      selections[i].style.cssText ='box-shadow: 2px 2px -5px rgb(0, 0, 0.2);';
+    }
+  });
+  // OtherWise Making A new For Loop For Nothing I Used The Same For Loop For Collapsing 
+  // To Make Evry Section Button To Head To It's Section (maping) 
+  Buttons[i+2].addEventListener("click",()=>
+  {
+    let posY = selections[i].offsetTop-35 ;
+    scroll(0,posY)
+  })
+}
+// Please Comment Me How Could It Be Better Even if This Project Accepted Thanks For You Time 
+
+
+
+
+
+
+
+  
